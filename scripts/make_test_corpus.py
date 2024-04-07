07bd6ef868a81c0f844767bc94e748b0d759bd66
danielzmbp <daniel.gomez-perez@uni-tuebingen.de>
2024-04-07 09:45:44 +0200

    notebooks new data update

--- scripts/make_test_corpus.py
+++ scripts/make_test_corpus.py
@@ -2,12 +2,12 @@ import more_itertools
 import os
 import polars as pl
 
-size = "1500"
+size = "1000"
 
 corpus = pl.read_parquet(
     '/home/gomez/biobert-pytorch/snakemake_pmc/pmc.parquet')
 
-corpus = corpus.filter(pl.col("text").str.len_chars() <= 512)
+# corpus = corpus.filter(pl.col("text").str.len_chars() <= 512)
 
 corpus_text = corpus["text"].to_list()
 
@@ -15,7 +15,7 @@ corpus_text = [i for i in corpus_text if i is not None]
 
 corpus_dir = f"corpus{size}"
 os.makedirs(corpus_dir, exist_ok=True)
-for i, batch in enumerate(more_itertools.batched(corpus_text, 300000)):
+for i, batch in enumerate(more_itertools.batched(corpus_text, 550000)):
     if (i < (int(size) + 1)):
         with open(f"{corpus_dir}/{i:>04}.txt", "w") as f:
             for s in batch:
