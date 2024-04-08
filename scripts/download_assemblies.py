230dc4b2b1e0b29db01efdfffb62951721dd47c5
danielzmbp <daniel.gomez-perez@uni-tuebingen.de>
2024-04-08 21:47:26 +0200

    add filter to download script

--- scripts/download_assemblies.py
+++ scripts/download_assemblies.py
@@ -4,30 +4,44 @@ import pandas as pd
 from tqdm import tqdm
 import subprocess
 import argparse
+import shutil
+from glob import glob
+import numpy as np
 
 Entrez.email = "d.gomez@lmu.de"
 Entrez.api_key = "71c734bb92382389e17af918de877c12b308"
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--max_assemblies", type=int, default=1500,
-                    help="Maximum number of assemblies")
-parser.add_argument("--data", type=int, help="Data value")
-parser.add_argument("--word_size_limit", type=int,
-                    default=3, help="Word size limit")
+parser.add_argument(
+    "--max_assemblies",
+    type=int,
+    default=500,
+    help="Maximum number of assemblies to download",
+)
+parser.add_argument("--data", type=int, help="Dataset value size")
+parser.add_argument(
+    "--word_size_limit", type=int, default=3, help="Assembly word name size limit"
+)
+parser.add_argument(
+    "--min_samples", type=int, default=3, help="Assemblies to filter per strain"
+)
 args = parser.parse_args()
 
+min_samples = args.min_samples
 max_assemblies = args.max_assemblies
 data = args.data
 word_size_limit = args.word_size_limit
+path = f"/home/gomez/gomez/assemblies/{data}/{max_assemblies}"
 
+## Download assemblies
 st = pd.read_parquet(f"../../gomez/preds{data}/REL_output/preds.parquet")
 
 nostrains = set()
-if os.path.exists('scripts/strain_cache.txt'):
-    with open('scripts/strain_cache.txt', 'r') as f:
+if os.path.exists("scripts/strain_cache.txt"):
+    with open("scripts/strain_cache.txt", "r") as f:
         nostrains = set(line.strip() for line in f)
 else:
-    os.system('touch scripts/strain_cache.txt')
+    os.system("touch scripts/strain_cache.txt")
     nostrains = []
 
 
@@ -38,14 +52,14 @@ def get_assembly_summary(id):
     return esummary_record
 
 
-def get_assemblies(term, download=True, path=f'/home/gomez/gomez/assemblies_linkbert_{max_assemblies}'):
+def get_assemblies(term, download=True, path=path):
     """
     Download genbank assemblies for a given search term.
 
     Args:
         term (str): The search term, usually the organism name.
         download (bool, optional): Whether to download the results. Defaults to True.
-        path (str, optional): The folder to save the assemblies to. Defaults to '/home/gomez/gomez/assemblies_linkbert_{max_assemblies}'.
+        path (str, optional): The folder to save the assemblies to. Defaults to '/home/gomez/gomez/assemblies/{max_assemblies}'.
 
     Returns:
         list: A list of FTP links to the downloaded assemblies.
@@ -54,9 +68,12 @@ def get_assemblies(term, download=True, path=f'/home/gomez/gomez/assemblies_link
     if term not in nostrains:
         if not (os.path.exists(f'{path}/{term.replace(" ","_")}/')):
             handle = Entrez.esearch(
-                db="assembly", term=f"({term} [ORGN]) AND (Bacteria [ORGN])", retmax=str(max_assemblies))
+                db="assembly",
+                term=f"({term} [ORGN]) AND (Bacteria [ORGN])",
+                retmax=str(max_assemblies),
+            )
             record = Entrez.read(handle, validate=False)
-            ids = record['IdList']
+            ids = record["IdList"]
             if len(ids) == 0:
                 if term not in nostrains:
                     with open("scripts/strain_cache.txt", "a") as f:
@@ -68,33 +85,61 @@ def get_assemblies(term, download=True, path=f'/home/gomez/gomez/assemblies_link
                     summary = get_assembly_summary(id)
                     # get ftp link
                     try:
-                        url = summary['DocumentSummarySet']['DocumentSummary'][0]['FtpPath_GenBank']
-                        if url == '':
+                        url = summary["DocumentSummarySet"]["DocumentSummary"][0][
+                            "FtpPath_GenBank"
+                        ]
+                        if url == "":
                             continue
                         label = os.path.basename(url)
                         # get the fasta link - change this to get other formats
-                        link = os.path.join(url, label+'_genomic.fna.gz')
+                        link = os.path.join(url, label + "_genomic.fna.gz")
                         print(f"\n{term}->{id}")
                         links.append(link)
-                        term = term.replace('/', '_').replace(")", "_").replace(":", "_").replace(' ', '_').replace(
-                            "(", "_").replace("=", "_").replace("'", "_").replace(";", "_").replace(",", "_").replace("|", "_").replace('.', '_').replace('-', '_').replace("^", "_").replace("*", "_").replace('"', '_').replace('___', '_').replace('__', '_').lstrip('_').rstrip('_')
-                        os.makedirs(f'{path}/{term}/{id}/', exist_ok=True)
+                        term = (
+                            term.replace("/", "_")
+                            .replace(")", "_")
+                            .replace(":", "_")
+                            .replace(" ", "_")
+                            .replace("(", "_")
+                            .replace("=", "_")
+                            .replace("'", "_")
+                            .replace(";", "_")
+                            .replace(",", "_")
+                            .replace("|", "_")
+                            .replace(".", "_")
+                            .replace("-", "_")
+                            .replace("^", "_")
+                            .replace("*", "_")
+                            .replace('"', "_")
+                            .replace("___", "_")
+                            .replace("__", "_")
+                            .lstrip("_")
+                            .rstrip("_")
+                        )
+                        os.makedirs(f"{path}/{term}/{id}/", exist_ok=True)
                         if download == True:
-                            if os.path.exists(f'{path}/{term}/{id}/{label}.fna.gz'):
+                            if os.path.exists(f"{path}/{term}/{id}/{label}.fna.gz"):
                                 continue
                             # download link
                             else:
                                 subprocess.Popen(
-                                    ["wget", "-q", "-O", f'{path}/{term}/{id}/{label}.fna.gz', link])
+                                    [
+                                        "wget",
+                                        "-q",
+                                        "-O",
+                                        f"{path}/{term}/{id}/{label}.fna.gz",
+                                        link,
+                                    ]
+                                )
                     except:
                         pass
                 return links
             else:
-                print(f'\n {term} <- {len(ids)}')
+                print(f"\n {term} <- {len(ids)}")
         else:
-            print(f'\n {term} <- already exists')
+            print(f"\n {term} <- already exists")
     else:
-        print(f'\n {term} <- no results')
+        print(f"\n {term} <- no results")
 
 
 strains = list(st.word_strain_qc.value_counts().index)
@@ -108,3 +153,54 @@ for i in tqdm(ss, desc="strains", position=0):
         get_assemblies(i)
     except:
         pass
+
+## Filter assemblies
+filtered_path = f"{path}_{min_samples}"
+os.makedirs(filtered_path, exist_ok=True)
+
+for strain in tqdm(os.listdir(path)):
+    os.makedirs(f"{filtered_path}/{strain}", exist_ok=True)
+    assemblies = os.listdir(f"{path}/{strain}")
+    if len(assemblies) > max_assemblies:
+        selected_assemblies = np.random.choice(
+            assemblies, max_assemblies, replace=False
+        )
+        for assembly in selected_assemblies:
+            assembly_path = f"{path}/{strain}/{assembly}"
+            # Check file size in bytes
+            if os.path.getsize(glob(f"{assembly_path}/*.fna.gz")[0]) < 5120:
+                available_assemblies = [
+                    a
+                    for a in assemblies
+                    if os.path.getsize(glob(f"{path}/{strain}/{a}/*.fna.gz")[0]) >= 5120
+                ]
+                if available_assemblies:
+                    assembly = np.random.choice(available_assemblies)
+                    assembly_path = f"{path}/{strain}/{assembly}"
+                else:
+                    continue
+            shutil.copytree(
+                assembly_path,
+                f"{filtered_path}/{strain}/{assembly}",
+                dirs_exist_ok=True,
+            )
+    else:
+        for assembly in assemblies:
+            assembly_path = f"{path}/{strain}/{assembly}"
+            # Check file size in bytes
+            if os.path.getsize(glob(f"{assembly_path}/*.fna.gz")[0]) < 5120:
+                available_assemblies = [
+                    a
+                    for a in assemblies
+                    if os.path.getsize(glob(f"{path}/{strain}/{a}/*.fna.gz")[0]) >= 5120
+                ]
+                if available_assemblies:
+                    assembly = np.random.choice(available_assemblies)
+                    assembly_path = f"{path}/{strain}/{assembly}"
+                else:
+                    continue
+            shutil.copytree(
+                assembly_path,
+                f"{filtered_path}/{strain}/{assembly}",
+                dirs_exist_ok=True,
+            )
--- scripts/filter_assemblies.py
+++ scripts/filter_assemblies.py
@@ -6,11 +6,16 @@ import shutil
 from glob import glob
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--max_assemblies", type=int, default=5,
-                    help="Maximum number of assemblies")
+parser.add_argument(
+    "--max_assemblies", type=int, default=5, help="Maximum number of assemblies"
+)
 parser.add_argument("--data", type=int, help="Data value")
-parser.add_argument("--original_path", type=str, default='/home/gomez/gomez/assemblies_linkbert_500',
-                    help="Original path of assemblies")
+parser.add_argument(
+    "--original_path",
+    type=str,
+    default="/home/gomez/gomez/assemblies_linkbert_500",
+    help="Original path of assemblies",
+)
 
 args = parser.parse_args()
 
@@ -26,31 +31,50 @@ for strain in tqdm(os.listdir(original_path)):
     assemblies = os.listdir(f"{original_path}/{strain}")
     if len(assemblies) > max_assemblies:
         selected_assemblies = np.random.choice(
-            assemblies, max_assemblies, replace=False)
+            assemblies, max_assemblies, replace=False
+        )
         for assembly in selected_assemblies:
             assembly_path = f"{original_path}/{strain}/{assembly}"
             # Check file size in bytes
             if os.path.getsize(glob(f"{assembly_path}/*.fna.gz")[0]) < 5120:
-                available_assemblies = [a for a in assemblies if os.path.getsize(
-                    glob(f"{original_path}/{strain}/{a}/*.fna.gz")[0]) >= 5120]
+                available_assemblies = [
+                    a
+                    for a in assemblies
+                    if os.path.getsize(
+                        glob(f"{original_path}/{strain}/{a}/*.fna.gz")[0]
+                    )
+                    >= 5120
+                ]
                 if available_assemblies:
                     assembly = np.random.choice(available_assemblies)
                     assembly_path = f"{original_path}/{strain}/{assembly}"
                 else:
                     continue
             shutil.copytree(
-                assembly_path, f"{filtered_path}/{strain}/{assembly}",dirs_exist_ok = True)
+                assembly_path,
+                f"{filtered_path}/{strain}/{assembly}",
+                dirs_exist_ok=True,
+            )
     else:
         for assembly in assemblies:
             assembly_path = f"{original_path}/{strain}/{assembly}"
             # Check file size in bytes
             if os.path.getsize(glob(f"{assembly_path}/*.fna.gz")[0]) < 5120:
-                available_assemblies = [a for a in assemblies if os.path.getsize(
-                    glob(f"{original_path}/{strain}/{a}/*.fna.gz")[0]) >= 5120]
+                available_assemblies = [
+                    a
+                    for a in assemblies
+                    if os.path.getsize(
+                        glob(f"{original_path}/{strain}/{a}/*.fna.gz")[0]
+                    )
+                    >= 5120
+                ]
                 if available_assemblies:
                     assembly = np.random.choice(available_assemblies)
                     assembly_path = f"{original_path}/{strain}/{assembly}"
                 else:
                     continue
             shutil.copytree(
-                assembly_path, f"{filtered_path}/{strain}/{assembly}",dirs_exist_ok = True)
+                assembly_path,
+                f"{filtered_path}/{strain}/{assembly}",
+                dirs_exist_ok=True,
+            )
