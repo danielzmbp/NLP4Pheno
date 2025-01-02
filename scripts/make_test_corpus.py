be4d04b1af2260663199f3e7a809f4dcb69767ae
danielzmbp <daniel.gomez-perez@zmbp.uni-tuebingen.de>
2025-01-02 16:11:54 +0100

    snakemake latest release

--- scripts/make_test_corpus.py
+++ scripts/make_test_corpus.py
@@ -1,28 +1,29 @@
-import more_itertools
 import os
 import polars as pl
+import pyarrow.parquet as pq
 
-size = "1100"
-
-corpus = pl.read_parquet(
-    '../snakemake_PMC/pmc.parquet')
-
-# corpus = corpus.filter(pl.col("text").str.len_chars() <= 512)
-
-corpus_text = corpus["text"].to_list()
-
-corpus_text = [i for i in corpus_text if i is not None]
+size = 2812
+pfile = pq.ParquetFile("./snakemake_PMC/pmc.parquet")
 
 corpus_dir = f"corpus{size}"
 os.makedirs(corpus_dir, exist_ok=True)
-for i, batch in enumerate(more_itertools.batched(corpus_text, 550000)):
-    if (i < (int(size) + 1)):
-        with open(f"{corpus_dir}/{i:>04}.txt", "w") as f:
-            for s in batch:
-                try:
-                    f.write(s)
-                    f.write("\n")
-                except:
-                    pass
-    else:
-        pass
+
+file_index = 0
+buffer_of_lines = []
+
+for batch in pfile.iter_batches(columns=["text"], batch_size=100000):
+    df = pl.from_arrow(batch).select("text").drop_nulls()
+    for line in df["text"].to_list():
+        buffer_of_lines.append(line)
+        if len(buffer_of_lines) >= 550000:
+            if file_index <= size:
+                with open(f"{corpus_dir}/{file_index:04}.txt", "w") as f:
+                    f.write("\n".join(buffer_of_lines))
+                file_index += 1
+            buffer_of_lines.clear()
+    if file_index > size:
+        break
+
+if buffer_of_lines and file_index <= size:
+    with open(f"{corpus_dir}/{file_index:04}.txt", "w") as f:
+        f.write("\n".join(buffer_of_lines))
